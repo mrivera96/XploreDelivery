b09295da30034236e745e1b8dc94d7b6dec81f75
mrivera96 <jylrivera96@gmail.com>
2020-05-10 08:18:02 -0600

    bk

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { CategoriesService } from 'src/app/services/categories.service';
-import { Category } from 'src/app/models/category';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Order } from 'src/app/models/order';
-import { formatDate } from '@angular/common';
-import { DeliveriesService } from 'src/app/services/deliveries.service';
-declare var $: any;
+import { Component, OnInit } from '@angular/core'
+import { CategoriesService } from 'src/app/services/categories.service'
+import { Category } from 'src/app/models/category'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Order } from 'src/app/models/order'
+import { formatDate } from '@angular/common'
+import { DeliveriesService } from 'src/app/services/deliveries.service'
+declare var $: any
 
 @Component({
   selector: 'app-home',
@@ -14,95 +14,103 @@ declare var $: any;
 })
 export class HomeComponent implements OnInit {
 
-  categories: Category[];
-  deliveryForm: FormGroup;
-  loading = false;
-  orders: Order[] = [];
-  currOrder: Order;
-  agregado = false;
-  exitMsg = '';
-  nDeliveryResponse;
-  errMsg='';
-
+  categories: Category[]
+  deliveryForm: FormGroup
+  loading = false
+  orders: Order[] = []
+  currOrder: Order
+  agregado = false
+  exitMsg = ''
+  nDeliveryResponse
+  errMsg = ''
+  
 
   constructor(
     private categoriesSService: CategoriesService,
     private formBuilder: FormBuilder,
-    private deliveriesService: DeliveriesService
+    private deliveriesService: DeliveriesService,
   ) { }
 
   ngOnInit(): void {
     this.deliveryForm = this.formBuilder.group({
-      deliveryHeader:  this.formBuilder.group({
+      deliveryHeader: this.formBuilder.group({
         nomCliente: ['', Validators.required],
         numIdentificacion: ['', [Validators.required, Validators.maxLength(14), Validators.minLength(13)]],
         numCelular: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
-        fecha: [formatDate(new Date, 'yyyy-MM-dd', 'en'), Validators.required],
-        hora: [formatDate(new Date, 'hh:mm', 'en'), Validators.required],
+        fecha: [formatDate(new Date(), 'yyyy-MM-dd', 'en'), Validators.required],
+        hora: [formatDate(new Date(), 'HH:mm', 'en'), Validators.required],
         dirRecogida: ['', Validators.required],
-        email: ['', [Validators.required,  Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
+        email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
         idCategoria: [null, Validators.required],
       }),
-     
+
       orders: this.formBuilder.group({
         nFactura: ['', Validators.required],
         nomDestinatario: ['', Validators.required],
         numCel: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
-        direccion: ['', Validators.required]
+        direccion: ['', Validators.required],
+        distancia:['']
       })
     });
 
-    this.loadData();
+    this.loadData()
   }
 
   loadData() {
     this.categoriesSService.getAllCategories().subscribe(response => {
-      this.categories = response.data;
-    });
+      this.categories = response.data
+    })
   }
 
-  onOrderAdd(){
-    if(this.deliveryForm.get('orders').valid){
-      this.agregado = true;
+  onOrderAdd() {
+  
+    if (this.deliveryForm.get('orders').valid) {
+      this.agregado = true
       this.currOrder = {
         nFactura: this.deliveryForm.get('orders').get('nFactura').value,
         nomDestinatario: this.deliveryForm.get('orders').get('nomDestinatario').value,
         numCel: this.deliveryForm.get('orders').get('numCel').value,
-        direccion: this.deliveryForm.get('orders').get('direccion').value
-      };
+        direccion: this.deliveryForm.get('orders').get('direccion').value,
+      }
 
-      this.orders.push(this.currOrder);
-      this.deliveryForm.get('orders').reset();
+      this.orders.push(this.currOrder)
+      this.deliveryForm.get('orders').reset()
 
-      setTimeout( () => {  this.agregado = false; }, 2000 );
-      
-    
+      setTimeout(() => { this.agregado = false; }, 2000)
     }
   }
 
-  onFormSubmit(){
-    if(this.deliveryForm.get('deliveryHeader').valid && this.orders.length > 0){
+  onFormSubmit() {
+    /*if (this.deliveryForm.get('deliveryHeader').valid && this.orders.length > 0) {
       this.loading = true;
       this.deliveriesService.newDelivery(this.deliveryForm.get('deliveryHeader').value, this.orders).subscribe(response => {
         this.deliveryForm.reset();
-        this.orders=[];
+        this.orders = [];
         this.loading = false;
-        if(response.error == 0){
-          this.exitMsg = response.message;
-          this.nDeliveryResponse = response.nDelivery;
-          const showModal: HTMLElement = document.getElementById('showSuccsModal') as HTMLElement;
-          showModal.click();
-        }
+       
+          this.exitMsg = response.message
+          this.nDeliveryResponse = response.nDelivery
+          const showModal: HTMLElement = document.getElementById('showSuccsModal') as HTMLElement
+          showModal.click()
       },
-      error => {
-        this.loading =false;
-        //this.deliveryForm.reset();
-        //this.orders=[];
-        const showModal: HTMLElement = document.getElementById('showErrModal') as HTMLElement;
-          showModal.click();
-      });
-    }
+        error => {
+          this.loading = false
+          //this.deliveryForm.reset();
+          //this.orders=[];
+          const showModal: HTMLElement = document.getElementById('showErrModal') as HTMLElement
+          showModal.click()
+        });
+        
+    }*/
+    console.log(this.deliveryForm.value)
 
   }
+
+
+  removeFromArray(item){
+    let i = this.orders.indexOf(item)
+    this.orders.splice(i, 1)
+  }
+
 
 }
--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -4,4 +4,5 @@ export interface Order {
     nomDestinatario?: string;
     numCel?: string;
     direccion?: string;
+    distancia?: string;
 }
